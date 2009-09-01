32b5f8abf81afd3363c4f9d2c1f74b46db0aaf49
Frank O'Connor <focon@xs4all.nl>
2009-09-01 15:34:57 +0200

    csv changes

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -4,29 +4,7 @@ class Admin::HomesController < ApplicationController
 def index
 
 end
-    require 'csv'
 
-   def csv_import 
-     @parsed_file=CSV::Reader.parse(params[:dump][:file])
-     n=0
-     @parsed_file.each  do |row|
-     c=Publication.new
-     
-     c.url=row[1]
-     c.title=row[2]
-     c.author=row[3]
-     c.venue=row[4]
-     c.year=row[5]
-     c.category=row[6]
-     if c.save
-        n=n+1
-        GC.start if n%50==0
-     end
-     flash.now[:message]="CSV Import Successful,  #{n} new
-
-                                records added to data base"
-   end
-end
 
   def authenticate
     authenticate_or_request_with_http_basic do |user, pass|
@@ -34,6 +12,6 @@ end
       user == 'akita' && pass == 'akita'      
     end
   end
- end
+end
 
 
