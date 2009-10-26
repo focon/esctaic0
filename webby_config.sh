13e9660683b7565b102aea031c413595d5ce76fa
focon <focon@xs4all.nl>
2009-10-26 23:16:14 +0100

    update webby_config.sh yet again and again

--- webby_config.sh
+++ webby_config.sh
@@ -1,35 +1,36 @@
-
-echo "gem: --no-ri --no-rdoc" >> ~/gem.rc
+ sudo gem install rails --version=2.3.4 --no-rdoc --no-ri
+ 
+ echo "gem: --no-ri --no-rdoc" >> ~/gem.rc
 
  # install required gems
-sudo gem install -v 1.9.0 liquid
-sudo gem install tzinfo test-unit
-sudo gem install authlogic
-sudo gem install lockdown
-sudo gem install mislav-will_paginate
+ sudo gem install -v 1.9.0 liquid
+ sudo gem install tzinfo test-unit
+ sudo gem install authlogic
+ sudo gem install lockdown
+ sudo gem install mislav-will_paginate
  rake gems:install
-echo WC_DB_ENGINE=${WC_DB_ENGINE}
+ echo WC_DB_ENGINE=${WC_DB_ENGINE}
  
-echo "
-login: &login
-  adapter: ${WC_DB_ENGINE}
-  database: ${WC_APP_NAME}
-  username: ${WC_APP_NAME}
-  password: ${WC_DB_PASSWORD}
-  host: localhost
+ echo "
+ login: &login
+   adapter: ${WC_DB_ENGINE}
+   database: ${WC_APP_NAME}
+   username: ${WC_APP_NAME}
+   password: ${WC_DB_PASSWORD}
+   host: localhost
   " > config/database.yml
  
-if [ "${WC_DB_ENGINE}" == "mysql" ]; then
-echo "
-  production:
-  <<: *login
-  encoding: utf8
-  " >> config/database.yml
-  fi
+ if [ "${WC_DB_ENGINE}" == "mysql" ]; then
+ echo "
+ production:
+    <<: *login
+    encoding: utf8
+   " >> config/database.yml
+   fi
  
 
  
-rake db:bootstrap RAILS_ENV=production
+ rake db:bootstrap RAILS_ENV=production
  rake config/initializers/session_store.rb
   rake db:bootstrap RAILS_ENV=production
   rake db:schema:load RAILS_ENV=production
