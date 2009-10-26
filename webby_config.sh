28beedc62e5c1715f1df1251ff5e8486e36a292e
focon <focon@xs4all.nl>
2009-10-26 16:36:22 +0100

    update webby_config.sh

--- webby_config.sh
+++ webby_config.sh
@@ -12,19 +12,19 @@ echo WC_DB_ENGINE=${WC_DB_ENGINE}
  
 echo "
 login: &login
-adapter: ${WC_DB_ENGINE}
-database: ${WC_APP_NAME}
-username: ${WC_APP_NAME}
-password: ${WC_DB_PASSWORD}
-host: localhost
-" > config/database.yml
+  adapter: ${WC_DB_ENGINE}
+  database: ${WC_APP_NAME}
+  username: ${WC_APP_NAME}
+  password: ${WC_DB_PASSWORD}
+  host: localhost
+  " > config/database.yml
  
 if [ "${WC_DB_ENGINE}" == "mysql" ]; then
 echo "
-production:
-<<: *login
-encoding: utf8
-" >> config/database.yml
+  production:
+  <<: *login
+  encoding: utf8
+  " >> config/database.yml
 fi
  
 
